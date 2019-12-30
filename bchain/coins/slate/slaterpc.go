3f83e0ec433d56617e8e1d13eadc6d4731b09e41
Oleg Mitrakhovich <olegmit9@hotmail.com>
2019-12-30 13:36:31 -0500

    added slateparser.go, slaterpc.go, config slate.json
    ...still missing all tests. also unable to find verification signature hash.

---
+++ bchain/coins/slate/slaterpc.go
@@ -0,0 +1,52 @@
+package slate
+
+import (
+	"blockbook/bchain"
+	"blockbook/bchain/coins/btc"
+	"encoding/json"
+
+	"github.com/golang/glog"
+)
+
+// SlateRPC is an interface to JSON-RPC bitcoind service.
+type SlateRPC struct {
+	*btc.BitcoinRPC
+}
+
+// NewSlateRPC returns new SlateRPC instance.
+func NewSlateRPC(config json.RawMessage, pushHandler func(bchain.NotificationType)) (bchain.BlockChain, error) {
+	b, err := btc.NewBitcoinRPC(config, pushHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &SlateRPC{
+		b.(*btc.BitcoinRPC),
+	}
+	s.RPCMarshaler = btc.JSONMarshalerV1{}
+	s.ChainConfig.SupportsEstimateFee = true
+	s.ChainConfig.SupportsEstimateSmartFee = false
+
+	return s, nil
+}
+
+// Initialize initializes PivXRPC instance.
+func (b *SlateRPC) Initialize() error {
+	chainName, err := b.GetChainInfoAndInitializeMempool(b)
+	if err != nil {
+		return err
+	}
+
+	glog.Info("Chain name ", chainName)
+	params := GetChainParams(chainName)
+
+	// always create parser
+	b.Parser = NewSlateParser(params, b.ChainConfig)
+
+	b.Testnet = false
+	b.Network = "livenet"
+
+	glog.Info("rpc: block chain ", params.Name)
+
+	return nil
+}
